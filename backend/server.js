b0717cf3203b4bcc498a30614dc3f091dd152db0
오세영 <leenight5309@naver.com>
2025-10-25 07:40:53 +0900

    test : 거의 마지막

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const PORT = 8080;
 app.use(cors());
 app.use(express.json());
 // frontend 폴더를 정적 파일 서비스 경로로 사용
-app.use(express.static(path.join(__dirname, 'frontend'))); 
+app.use(express.static(path.join(__dirname, 'public'))); 
 
 // ============================
 // 학교 검색 API
